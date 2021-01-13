523ac66c14ce4a2fd68a7beea5398f1850f3cdc1
James Requeima <jamesr@pop-os.localdomain>
2021-01-13 15:03:16 -0600

    Added a bit more to code method.

--- neuralprocesses/chain.py
+++ neuralprocesses/chain.py
@@ -13,13 +13,30 @@ class Chain:
             x = link(x)
         return x
 
+class Parallel:
+    def __init__(self, *elements):
+        self.elements
+
 
 @_dispatch(Chain, B.Numeric, B.Numeric, B.Numeric)
 def code(c, xz, z, x, **kw_args):
     for ci in c.links:
-            xz, z = code(ci, xz, z, x, **kw_args)
+        xz, z = code(ci, xz, z, x, **kw_args)
     return xz, z
 
+@_dispatch(Parallel, B.Numeric, B.Numeric, B.Numeric)
+def code(p, xz, z, x, **kw_args):
+    xz, z = zip([code(pi, xz, z, x, **kw_args) for pi in p.elements])
+    return Parallel(xz), Parallel(z)
 
+@_dispatch(Parallel, B.Numeric, Parallel, B.Numeric)
+def code(p, xz, z, x, **kw_args):
+    xz, z = zip([code(pi, xz, zi, x, **kw_args) for (pi, zi) in zip(p.elements, z.elements)])
+    return Parallel(xz), Parallel(z)
 
+@_dispatch(Parallel, Parallel, Parallel, B.Numeric)
+def code(p, xz, z, x, **kw_args):
+    xz, z = zip([code(pi, xzi, zi, x, **kw_args) for (pi, xzi, zi) 
+        in zip(p.elements, zi.elements,  z.elements)])
+    return Parallel(xz), Parallel(z)
 
