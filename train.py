6f0a9ae5af0c5c05aad53d52c6a00c03e05014db
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-05-13 18:23:27 +0100

    Add moving average

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import lab.torch as B
+import numpy as np
 import torch
 
 from neuralprocesses.data import GPGenerator
@@ -15,21 +16,31 @@ model = GNP().to(device)
 
 opt = torch.optim.Adam(model.parameters(), lr=5e-4)
 
+
+class MovingAverage:
+    def __init__(self, size=50):
+        self.stack = []
+        self.size = size
+
+    def record(self, value):
+        self.stack.append(B.to_numpy(value))
+        while len(self.stack) > self.size:
+            self.stack.pop(0)
+        print(B.mean(np.array(self.stack)))
+
+
+ma = MovingAverage()
+
 for batch in gen.epoch(device):
     mean, cov = model(batch["x_context"], batch["y_context"], batch["x_target"])
     dist = torch.distributions.MultivariateNormal(loc=mean, covariance_matrix=cov)
-    loss = -B.mean(dist.log_prob(batch["y_target"][:, :, 0])) / B.shape(batch["y_target"])[1]
-    print(loss)
+    loss = (
+        -B.mean(dist.log_prob(batch["y_target"][:, :, 0]))
+        / B.shape(batch["y_target"])[1]
+    )
+
+    ma.record(loss)
 
     loss.backward()
     opt.step()
     opt.zero_grad()
-
-
-
-
-
-
-
-
-
