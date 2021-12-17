56109170737afdc73581b8c2649f427d30a0b33e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-12-17 16:54:23 +0100

    Add option to select number of harmonics

--- train_gp.py
+++ train_gp.py
@@ -11,13 +11,15 @@ parser.add_argument("--dim_x", type=int, default=1)
 parser.add_argument("--dim_y", type=int, default=1)
 parser.add_argument("--backend", choices=["tensorflow", "torch"], required=True)
 parser.add_argument("--batch_size", type=int, default=32)
-parser.add_argument("--harmonics", action="store_true")
+parser.add_argument("--harmonics", type=int, default=0)
 args = parser.parse_args()
 
 batch_size = args.batch_size
 rate = 1e-3 * B.sqrt(args.batch_size / 16)
 dim_x = args.dim_x
 dim_y = args.dim_y
+harmonics_range = (-2, 2) if args.harmonics > 0 else None
+num_harmonics = args.harmonics
 
 if args.backend == "torch":
 
@@ -77,7 +79,8 @@ model = to_device(
         dim_x=dim_x,
         dim_y=dim_y,
         likelihood="lowrank",
-        harmonics_range=(-2, 2) if args.harmonics else None,
+        harmonics_range=harmonics_range,
+        num_harmonics=num_harmonics,
     )
 )
 
