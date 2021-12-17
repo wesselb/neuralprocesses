52299108307c801157766fbfa5f945c48a2a6509
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-12-17 18:10:52 +0100

    Add eval numbers

--- train_gp.py
+++ train_gp.py
@@ -38,11 +38,12 @@ if args.backend == "torch":
         return backend.optim.Adam(model.parameters(), rate)
 
     def step_optimiser(opt, model, f):
-        val = f()
+        vals = f()
+        val = B.mean(vals)
         opt.zero_grad(set_to_none=True)
         val.backward()
         opt.step()
-        return val
+        return vals
 
 elif args.backend == "tensorflow":
 
@@ -62,10 +63,11 @@ elif args.backend == "tensorflow":
 
     def step_optimiser(opt, model, f):
         with backend.GradientTape() as tape:
-            val = f()
+            vals = f()
+            val = B.mean(vals)
         grads = tape.gradient(val, model.trainable_weights)
         opt.apply_gradients(zip(grads, model.trainable_weights))
-        return val
+        return vals
 
 else:
     raise ValueError(f'Unknown backend "{args.backend}".')
@@ -97,13 +99,32 @@ gen = GPGenerator(
     pred_logpdf_diag=True,
     device=device,
 )
+gen_eval = GPGenerator(
+    backend.float32,
+    kernel=EQ().stretch(0.25 * B.sqrt(2) ** (dim_x - 1)),
+    num_tasks=4096,
+    batch_size=16,
+    num_context_points=(3, 10),
+    num_target_points=50,
+    x_ranges=((-2, 2),) * dim_x,
+    dim_y=dim_y,
+    pred_logpdf=True,
+    pred_logpdf_diag=True,
+    device=device,
+)
 
 
 def objective(xc, yc, xt, yt):
     pred = model(xc, yc, xt)
     # Use `float64`s for the logpdf computation.
     pred = B.cast(backend.float64, pred)
-    return -B.mean(pred.logpdf(B.cast(backend.float64, yt)))
+    return -pred.logpdf(B.cast(backend.float64, yt))
+
+
+def with_err(vals):
+    mean = B.mean(vals)
+    err = 1.96 * B.std(vals) / B.sqrt(B.length(vals))
+    return f"{mean:7.3f} +- {err:7.3f}"
 
 
 opt = create_optimiser(model)
@@ -112,7 +133,7 @@ with Progress(name="Epochs", total=10_000) as progress_epochs:
     for i in range(10_000):
         with Progress(name=f"Epoch {i + 1}", total=gen.num_batches) as progress_epoch:
             for batch in gen.epoch():
-                val = step_optimiser(
+                vals = step_optimiser(
                     opt,
                     model,
                     lambda: objective(
@@ -125,8 +146,25 @@ with Progress(name="Epochs", total=10_000) as progress_epochs:
                 nt = B.shape(batch["xt"], 2)
                 progress_epoch(
                     {
-                        "KL (full)": (val + B.mean(batch["pred_logpdf"])) / nt,
-                        "KL (diag)": (val + B.mean(batch["pred_logpdf_diag"])) / nt,
+                        "KL (full)": with_err((vals + batch["pred_logpdf"]) / nt),
+                        "KL (diag)": with_err((vals + batch["pred_logpdf_diag"]) / nt),
                     }
                 )
-        progress_epochs()
+
+        vals = []
+        for batch in gen.epoch():
+            vals.append(
+                objective(
+                    batch["xc"],
+                    batch["yc"],
+                    batch["xt"],
+                    batch["yt"],
+                )
+            )
+        vals = B.stack(*vals)
+        progress_epochs(
+            {
+                "KL (full)": with_err((vals + batch["pred_logpdf"]) / nt),
+                "KL (diag)": with_err((vals + batch["pred_logpdf_diag"]) / nt),
+            }
+        )
