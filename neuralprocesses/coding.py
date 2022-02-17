d42f0890e7d67b75d13604df4ca6e01eaae97611
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-02-17 19:55:24 +0100

    Fix style

--- neuralprocesses/coding.py
+++ neuralprocesses/coding.py
@@ -36,6 +36,7 @@ def _merge(z0: B.Numeric, *zs: B.Numeric):
             raise ValueError("Cannot merge inputs.")
     return zs[0]
 
+
 @_dispatch
 def _merge(z0: tuple, *zs: tuple):
     zs = (z0,) + zs
