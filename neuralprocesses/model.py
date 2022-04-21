132798d7a522f47e80bf4f85a8387a32ff62cbc8
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-04-21 12:08:39 +0200

    Fix elbo

--- neuralprocesses/model.py
+++ neuralprocesses/model.py
@@ -298,7 +298,7 @@ def elbo(
     state = B.global_random_state(B.dtype(xt))
     state, elbos = elbo(state, model, xc, yc, xt, yt, **kw_args)
     B.set_global_random_state(state)
-    return elbo
+    return elbos
 
 
 @_dispatch
