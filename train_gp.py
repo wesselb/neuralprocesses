f0424d10da6b2ce6129d2f42a47bb45f6f7b4fb1
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-04-21 14:15:05 +0200

    Fix range

--- train_gp.py
+++ train_gp.py
@@ -291,9 +291,9 @@ gens_eval = [
         )[args.data],
     )
     for name, x_range_context, x_range_target in [
-        ("interpolation in training range", (-2, 2), (-2, -2)),
+        ("interpolation in training range", (-2, 2), (-2, 2)),
         ("interpolation beyond training range", (2, 6), (2, 6)),
-        ("extrapolation beyond training range", (-2, 2), (2, 6)),
+        ("extrapolation beyond training range", (0, 2), (2, 4)),
     ]
 ]
 
