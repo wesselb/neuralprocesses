69ad493342f319deafcc5846b09ea9b825a118d9
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-04-21 14:25:23 +0200

    Fix bug in loglik

--- neuralprocesses/model.py
+++ neuralprocesses/model.py
@@ -185,6 +185,10 @@ def loglik(
     float = B.dtype_float(yt)
     float64 = B.promote_dtypes(float, np.float64)
 
+    # If `num_samples = 1`, then there will not be a sample dimension, so we can
+    # avoid the `logsumexp`.
+    do_logsumexp = num_samples > 1
+
     # Sample in batches to alleviate memory requirements.
     logpdfs = None
     done_num_samples = 0
@@ -206,9 +210,11 @@ def loglik(
         this_logpdfs = pred.logpdf(B.cast(float64, yt))
 
         # If the number of samples is equal to one but `num_samples > 1`, then the
-        # likelihood was a `Dirac`, so we can stop batching.
+        # likelihood was a `Dirac`, so we can stop batching. Also, set `num_samples = 1`
+        # because we only have one sample now.
         if num_samples > 1 and B.shape(this_logpdfs, 0) == 1:
             logpdfs = this_logpdfs
+            num_samples = 1
             break
 
         # Record current samples.
@@ -222,9 +228,9 @@ def loglik(
         done_num_samples += this_num_samples
 
     # Average over samples.
-    if num_samples > 1:
+    if do_logsumexp:
         # Sample dimension should always be the first.
-        logpdfs = B.logsumexp(logpdfs, axis=0) - B.log(num_samples)
+        logpdf = B.logsumexp(logpdfs, axis=0) - B.log(num_samples)
 
     if normalise:
         # Normalise by the number of targets.
