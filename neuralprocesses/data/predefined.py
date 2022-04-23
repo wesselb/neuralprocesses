00f962af73e0ded17c2c774bfb228f4dc9570b36
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-04-23 14:00:23 +0200

    Scale contexts and targets with dim

--- neuralprocesses/data/predefined.py
+++ neuralprocesses/data/predefined.py
@@ -64,8 +64,8 @@ def construct_predefined_gens(
             dtype,
             noise=0.05,
             kernel=kernel,
-            num_context_points=(0, 50),
-            num_target_points=50,
+            num_context_points=(0, 20 * dim_x),
+            num_target_points=50 * dim_x,
             pred_logpdf=pred_logpdf,
             pred_logpdf_diag=pred_logpdf_diag,
             **config,
@@ -77,8 +77,8 @@ def construct_predefined_gens(
         # The sawtooth is hard already as it is. Do not add noise.
         noise=0,
         freqs=(2 / factor, 4 / factor),
-        num_context_points=(0, 100),
-        num_target_points=100,
+        num_context_points=(0, 50 * dim_x),
+        num_target_points=100 * dim_x,
         **config,
     )
     gens["mixture"] = MixtureGenerator(
