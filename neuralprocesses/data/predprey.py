7f46fb201649a2c8ebecdf09c4c7ecc94e09f0ba
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-04-28 16:14:19 +0200

    Improve PredPreyGenerator

--- neuralprocesses/data/predprey.py
+++ neuralprocesses/data/predprey.py
@@ -1,29 +1,37 @@
 import lab as B
+import wbml.util
 
-from .data import SyntheticGenerator
+from .data import SyntheticGenerator, _sample_all_inputs_noise
 
 __all__ = ["PredPreyGenerator"]
 
 
-def _predprey_step(state, x_y, t, dt, *, alpha, beta, delta, gamma, sigma, sigma_floor):
+def _predprey_step(state, x_y, t, dt, *, alpha, beta, delta, gamma, sigma):
     x = x_y[..., 0]
     y = x_y[..., 1]
 
     m = 25
 
-    deriv_x = x * (alpha - beta * y) * (1 - x / m)
-    deriv_y = y * (-delta + gamma * x) * (1 - y / m)
-
-    state, randn = B.randn(state, B.dtype(x), 4, *B.shape(x))
+    state, randn = B.randn(state, B.dtype(x), 2, *B.shape(x))
     dw = B.sqrt(dt) * randn
 
-    x = x + deriv_x * dt
-    x = x + x * (1 - x / m) * sigma * dw[0] + sigma_floor * dw[1]
-    y = y + deriv_y * dt
-    y = y - y * (1 - y / m) * sigma * dw[2] + sigma_floor * dw[3]
+    deriv_x = x * (alpha - beta * y) * (1 - x / m)
+    deriv_y = y * (-delta + gamma * x) * (1 - y / m)
+    # Apply an exponent 0.2 to emphasise the noise at lower population levels and
+    # prevent the populations from dying out.
+    x = x + deriv_x * dt + (x**0.2) * (1 - x / m) * sigma * dw[0]
+    y = y + deriv_y * dt - (y**0.2) * (1 - y / m) * sigma * dw[1]
 
-    x = B.maximum(x, B.zero(x))
-    y = B.maximum(y, B.zero(y))
+    # Make sure that the populations never become negative. Mathematically, the
+    # populations should remain positive. Note that if we were to `max(x, 0)`, then
+    # `x` could become zero. We therefore take the absolute value.
+    x = B.abs(x)
+    y = B.abs(y)
+
+    # Cap the populations by `m`. Mathematically, they should never rise above `m`, but
+    # numerically they can due to the non-zero step size.
+    x = B.minimum(x, m * B.one(x))
+    y = B.minimum(y, m * B.one(y))
 
     t = t + dt
 
@@ -38,8 +46,7 @@ def _predprey_rand_params(state, dtype, batch_size=16):
     delta = 0.25 + rand[2]
     gamma = delta * (0.2 + 0.2 * rand[3])
 
-    sigma = 0.05 + 0.1 * rand[4]
-    sigma_floor = 1
+    sigma = 0.2 + 0.6 * rand[4]
 
     return state, {
         "alpha": alpha,
@@ -47,27 +54,41 @@ def _predprey_rand_params(state, dtype, batch_size=16):
         "delta": delta,
         "gamma": gamma,
         "sigma": sigma,
-        "sigma_floor": sigma_floor,
     }
 
 
-def _predprey_simulate(state, dtype, t0, t1, dt, n_out, *, batch_size=16):
+def _predprey_simulate(state, dtype, t0, t1, dt, t_target, *, batch_size=16):
     state, params = _predprey_rand_params(state, dtype, batch_size=batch_size)
 
+    # Sort the target times for the validity of the loop below.
+    perm = B.argsort(t_target)
+    inv_perm = wbml.util.inv_perm(perm)
+    t_target[perm]
+
+    # Note the magic constant 10 here.
     x_y = 10 * B.rand(dtype, batch_size, 2)
     t = t0
-    t_out = t0
     traj, ts = [x_y], [t]
 
     while t < t1:
         state, x_y, t = _predprey_step(state, x_y, t, dt, **params)
-        if t >= t_out:
+        while B.shape(t_target, 0) > 0 and t >= t_target[0]:
             traj.append(x_y)
             ts.append(t)
-            t_out += (t1 - t0) / n_out
+            t_target = t_target[1:]
 
     t = B.tile(B.stack(*ts)[None, None, :], batch_size, 1, 1)
-    traj = 7 * B.stack(*traj, axis=-1)
+    # Note the magic scaling `7 / 100` here.
+    traj = B.stack(*traj, axis=-1) * 7 / 100
+
+    # Undo the sorting.
+    t = B.take(t, inv_perm, axis=-1)
+    traj = B.take(traj, inv_perm, axis=-1)
+
+    # We now apply a random scaling and offset.
+    state, offset = B.rand(state, dtype, batch_size, 2, 1)
+    state, scale = B.rand(state, dtype, batch_size, 2, 1)
+    traj = 0.25 * offset + (0.5 + scale) * traj
 
     return state, t, traj
 
@@ -77,15 +98,15 @@ class PredPreyGenerator(SyntheticGenerator):
 
     Further takes in arguments and keyword arguments from the constructor of
     :class:`.data.SyntheticGenerator`. Moreover, also has the attributes of
-    :class:`.data.SyntheticGenerator`.
-
+    :class:`.data.SyntheticGenerator`. However, the defaults for this class are
+    different.
     """
 
     def __init__(
         self,
         *args,
         x_ranges=((0, 100),),
-        num_context_points=(100, 100),
+        num_context_points=(50, 100),
         num_target_points=(100, 100),
         dim_y=2,
         **kw_args
@@ -100,15 +121,11 @@ class PredPreyGenerator(SyntheticGenerator):
         )
         if not (self.dim_x == 1 and self.dim_y == 2):
             raise RuntimeError("`dim_x` must be 1 and `dim_y` must be 2.")
-        if not (
-            B.all(self.x_ranges_context[i] == self.x_ranges_target[i]) for i in [0, 1]
-        ):
-            raise RuntimeError("`x_ranges_context` must be equal to `x_ranges_target`.")
-
         self._big_batch = None
         self._big_batch_num_left = 0
 
     def generate_batch(self):
+        # Attempt to return a batch from the big batch.
         if self._big_batch_num_left > 0:
             n = self.batch_size
             batch = {k: v[:n] for k, v in self._big_batch.items()}
@@ -117,43 +134,39 @@ class PredPreyGenerator(SyntheticGenerator):
             return batch
 
         with B.on_device(self.device):
+            # For computational efficiency, we will not generate one batch, but
+            # `multiplier` many batches.
             multiplier = max(1024 // self.batch_size, 1)
+
             batch = {}
 
+            # Sample inputs
+            x, num_context_points, noise = _sample_all_inputs_noise(self)
+            # We fix the inputs across all tasks, because that is what the simulator
+            # requires.
+            x = x[0, 0]
+
             # Simulate the equations.
-            self.state, x, y = _predprey_simulate(
+            self.state, _, y = _predprey_simulate(
                 self.state,
                 self.float64,
-                float(self.x_ranges_context[0][0]),
-                float(self.x_ranges_context[1][0]),
+                min(self.x_ranges_context[0][0], self.x_ranges_target[0][0]),
+                max(self.x_ranges_context[1][0], self.x_ranges_target[1][0]),
                 7 / 365,
-                200,
+                x,
                 batch_size=multiplier * self.batch_size,
             )
-
-            # Sample numbers of context and target points.
-            lower, upper = self.num_context_points
-            self.state, num_context_points = B.randint(
-                self.state, self.int64, lower=lower, upper=upper + 1
-            )
-            lower, upper = self.num_target_points
-            self.state, num_target_points = B.randint(
-                self.state, self.int64, lower=lower, upper=upper + 1
-            )
-
-            self.state, perm = B.randperm(self.state, self.int64, 200)
-            x = x[..., perm[: num_context_points + num_target_points]]
-            y = y[..., perm[: num_context_points + num_target_points]]
-
-            if B.any(B.isnan(y)):
-                return self.generate_batch()
+            # Now make the inputs of the right size.
+            x = B.tile(x[None, None, :], multiplier * self.batch_size, 1, 1)
 
             batch["xc"] = B.cast(self.dtype, x[:, :, :num_context_points])
             batch["yc"] = B.cast(self.dtype, y[:, :, :num_context_points])
             batch["xt"] = B.cast(self.dtype, x[:, :, num_context_points:])
             batch["yt"] = B.cast(self.dtype, y[:, :, num_context_points:])
 
+            # Save the big batch.
             self._big_batch_num_left = multiplier
             self._big_batch = batch
 
+            # Call the function again to obtain a batch from the big batch.
             return self.generate_batch()
