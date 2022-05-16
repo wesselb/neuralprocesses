0b52271b5aab21a46a029be9dbec7801be96b68e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-05-17 01:31:38 +0200

    Fix MAE check script

--- scripts/temperature_mae.py
+++ scripts/temperature_mae.py
@@ -7,19 +7,21 @@ import wbml.out as out
 from experiment.util import with_err
 from train import main
 
-sys.argv += "--load"
+sys.argv += ["--data", "temperature", "--load"]
 
 # Load experiment.
 exp = main()
 model = exp["model"]
 model.load_state_dict(torch.load(exp["wd"].file("model-last.torch"))["weights"])
 
-maes = []
 state = B.create_random_state(torch.float32, seed=0)
 
-for batch in exp["gen_cv"]:
-    state, pred = model(state, batch["contexts"], batch["xt"])
-    mask = ~B.isnan(batch["yt"])
-    maes.append(B.abs(pred.mean[mask] - batch["yt"][mask]))
+maes = []
+
+with torch.no_grad():
+    for batch in exp["gen_cv"]().epoch():
+        state, pred = model(state, batch["contexts"], batch["xt"])
+        mask = ~B.isnan(batch["yt"])
+        maes.append(B.abs(pred.mean[mask] - batch["yt"][mask]))
 
 out.kv("MAE", with_err(B.concat(*maes), and_upper=True))
