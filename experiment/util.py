f00fe88f6d519f155ce4f0a053e8f0e254d3eddd
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-05-28 18:14:56 +0200

    Add function to conveniently load models

--- experiment/util.py
+++ experiment/util.py
@@ -1,6 +1,7 @@
 import lab as B
+import torch
 
-__all__ = ["with_err"]
+__all__ = ["load", "with_err"]
 
 
 def with_err(vals, and_lower=False, and_upper=False):
@@ -14,3 +15,14 @@ def with_err(vals, and_lower=False, and_upper=False):
     if and_upper:
         res += f" ({mean + err:10.5f})"
     return res
+
+
+def load(last=False, device="cpu", **kw_args):
+    """Load an existing model."""
+    from train import main
+
+    exp = main(**kw_args, load=True)
+    wd = exp["wd"]
+    f = "model-last.torch" if last else "model-best.torch"
+    exp["model"].load_state_dict(torch.load(wd.file(f), map_location=device)["weights"])
+    return exp
