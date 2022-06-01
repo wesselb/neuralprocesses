5792dce735509d58c6e27a9c5566d0a1ce2821b4
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-06-01 10:06:41 +0200

    Fix formatting

--- scripts/temperature_mae.py
+++ scripts/temperature_mae.py
@@ -4,7 +4,6 @@ import lab as B
 import torch
 import wbml.out as out
 from neuralprocesses import AugmentedInput
-
 from train import main
 
 # Load experiment.
@@ -15,7 +14,9 @@ exp = main()
 model = exp["model"]
 model.load_state_dict(torch.load(exp["wd"].file("model-last.torch"))["weights"])
 
+
 def strip_augmentation(x):
+    """Strip possible augmentation from the inputs."""
     if isinstance(x, AugmentedInput):
         return x.x
     return x
@@ -25,15 +26,20 @@ xt_all = strip_augmentation(exp["gens_eval"]()[0][1].generate_batch()["xt"])[0]
 
 
 def reindex(mae, xt):
+    """Let the MAEs to correctly line up for randomly sampled batched."""
     nan_row = mae[:, :, :1] * B.nan
     xt = strip_augmentation(xt)[0]
-    rows = []
+
+    # Precomputing the distances like this allows us to get away with a simple
+    # `for`-loop below. No need to optimise that further.
     dists = B.to_numpy(B.pw_dists(B.t(xt_all), B.t(xt)).cpu())
+
+    rows = []
     for i in range(B.shape(xt_all, -1)):
         match = False
         for j in range(B.shape(xt, -1)):
             if dists[i, j] < 1e-6:
-                rows.append(mae[:, :, j: j+1])
+                rows.append(mae[:, :, j : j + 1])
                 match = True
                 break
         if not match:
