e46e4b5f5c637e7ddf1e92a906a11ccdf659096e
Anthony Buonomo <arb246@georgetown.edu>
2022-07-21 21:56:15 +0100

    add grid distrib

--- neuralprocesses/dist/uniform.py
+++ neuralprocesses/dist/uniform.py
@@ -1,6 +1,7 @@
 import lab as B
 from lab.shape import Dimension
 from plum import Tuple
+import torch
 
 from .dist import AbstractDistribution
 from .. import _dispatch
@@ -60,3 +61,47 @@ class UniformDiscrete(AbstractDistribution):
 
     def sample(self, state, dtype, *shape):
         return B.randint(state, dtype, lower=self.lower, upper=self.upper + 1)
+
+
+class Grid(AbstractDistribution):
+    """A uniform continuous distribution.
+
+    Also takes in tuples of its arguments.
+
+    Args:
+        lower (float): Lower bound.
+        upper (float): Upper bound.
+
+    Attributes:
+        lowers (vector): Lower bounds.
+        uppers (vector): Upper bounds.
+    """
+
+    @_dispatch
+    def __init__(self, lower: B.Number, upper: B.Number):
+        self.__init__((lower, upper))
+
+    @_dispatch
+    def __init__(self, *bounds: Tuple[B.Number, B.Number]):
+        lowers, uppers = zip(*bounds)
+        self.lowers = B.stack(*lowers)
+        self.uppers = B.stack(*uppers)
+
+    def sample(self, state, dtype, *shape):
+        # TODO: this is pretty broken for most uses probably, but I cannot figure
+        # out how to shape the tensor correct just using B. and B.cast won't
+        # conver the datatype
+        batch_size = shape[0]
+        n = shape[1]
+        lowers = B.to_active_device(B.cast(dtype, self.lowers))
+        uppers = B.to_active_device(B.cast(dtype, self.uppers))
+        grid = B.linspace(lowers, uppers, n)
+        # repeat the grid for each batch
+        grids = [grid for _ in range(batch_size)]
+        tg = torch.Tensor(grids)
+        # tg = B.to_active_device(B.cast(dtype, tg))
+        # B.cast(dtype, grids)
+        # Wrap everything in `Dimension`s to make dispatch work.
+        shape = (Dimension(d) for d in shape)
+        state, rand = B.rand(state, dtype, *shape, B.shape(lowers, 0))
+        return state, tg
