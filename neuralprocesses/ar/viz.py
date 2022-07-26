ba8d2842320b680a4c70034ea1e9cbcde5a768f7
Anthony Buonomo <arb246@georgetown.edu>
2022-07-26 16:20:41 +0100

    calc density on the fly

--- neuralprocesses/ar/viz.py
+++ neuralprocesses/ar/viz.py
@@ -1,12 +1,14 @@
 import logging
 from datetime import datetime
 from pathlib import Path
+from tqdm import tqdm
 
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 from time import time
 import seaborn as sns
+from scipy.stats import norm
 import yaml
 from matplotlib.patches import Rectangle
 import matplotlib.animation as animation
@@ -28,15 +30,141 @@ LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
 
+def get_function_predictive_params(
+        density_loc,
+        context_size,
+        function_index
+):
+    """
+    Load the means and variances for all trajectory length and number of trajectories
+    for the given function (as specific by context size and index).
+
+    Args:
+        density_loc: The location of the density data
+        context_size: The context size of the function
+        function_index: The index of the function
+
+    Returns:
+        A tuple of the means and variances, x target locations, and true y values
+            at those locations.
+    """
+    with h5py.File(density_loc, "r") as f:
+        mds = f[Groups.MARGINAL_DENSITIES.value]
+        grp = mds[f"{context_size}|{function_index}"]
+        xt = grp["x_targets"][:].reshape(-1)
+        yt = grp["y_density_evaluation_points"][:, 0]
+        # ^ first index contains true targets
+        means = grp[Datasets.MEANS.value][:]
+        variances = grp[Datasets.VARIANCES.value][:]
+    return means, variances, xt, yt
+
+
+def get_function_density(
+        means,
+        variances,
+        xt,
+        number_of_trajectories,
+        trajectory_length,
+        density_eval_locations,
+):
+    """
+    Get the density for each target point for the provided function parameters
+
+    Args:
+        means: The means of the predictive model for this function
+        variances:  The variances of the predictive model for this function
+        xt:  The target points
+        number_of_trajectories: The number of trajectories
+        trajectory_length: The length of each trajectory
+        density_eval_locations: The locations to evaluate the density at
+
+    Returns:
+        An array of densities for each target point with shape:
+            (number of density evaluation points, number of targets)
+    """
+    nt = number_of_trajectories
+    tl = trajectory_length
+
+    param_m0 = means[:nt, :, :, :, tl]
+    param_v0 = variances[:nt, :, :, :, tl]
+
+    gmms_per_target = get_gmms_per_target(
+        param_m0,
+        param_v0,
+        xt,
+        nt,
+        density_eval_locations,
+    )
+    return gmms_per_target
+
+
+def get_gmms_per_target(param_m0, param_v0, xt, nt, density_eval_locations):
+    """
+    Get the GMMs density for each target point.
+
+    Args:
+        param_m0: The means of the predictive model
+        param_v0: The variances of the predictive model
+        xt: The target points
+        nt: The number of trajectories
+        density_eval_locations: The locations to evaluate the density at
+
+    Returns:
+        An array of GMMs for each target point with shape:
+            (number of density evaluation points, number of targets)
+    """
+    num_density_eval_points = len(density_eval_locations)
+
+    gmms_per_target = np.zeros((num_density_eval_points, xt.shape[0]))
+    pbar = tqdm(enumerate(xt), total=xt.shape[0])
+    for i, xt0 in pbar:
+        gmm_components = np.zeros((nt, num_density_eval_points))
+        for nt0 in range(nt):
+            d = get_gmm_components(
+                param_m0,
+                param_v0,
+                nt0,
+                i,
+                density_eval_locations,
+            )
+            gmm_components[nt0, :] = d
+        gmm_d = gmm_components.mean(axis=0)
+        gmms_per_target[:, i] = gmm_d
+    return gmms_per_target
+
+
+def get_gmm_components(
+  param_m0, param_v0, nt0, i, density_eval_locations
+):
+    """
+    Get the GMM components for the density.
+
+    Args:
+        param_m0: means of the predictive model
+        param_v0: variances of the predictive model
+        nt0: the current number of trajectories
+        i: the current target point index
+        density_eval_locations: the locations to evaluate the density at
+
+    Returns:
+        A GMM component evaluated at each density evaluation point
+    """
+    component_m0 = param_m0[nt0, i, :, :]
+    component_v0 = param_v0[nt0, i, :, :]
+    d = norm.pdf(x=density_eval_locations, loc=component_m0, scale=np.sqrt(component_v0))
+    return d
+
+
 class MyAnimator:
-    def __init__(self, density_loc, num_contexts, func_ind):
+    def __init__(self, density_loc, num_contexts, func_ind, eval_points):
         self.density_loc = density_loc
         self.num_contexts = num_contexts
         self.func_ind = func_ind
 
+        self.eval_points = eval_points
+
         self.lh = None
         self.xt = None
-        self.eval_points = None
         self.true_y_targets = None
         self.cx = None
         self.cy = None
@@ -49,6 +177,9 @@ class MyAnimator:
         self.targets = None
         self.density_eval_locations = None
 
+        self.means = None
+        self.variances = None
+
         self.vmax = None
         self.nlevels = None
 
@@ -56,7 +187,7 @@ class MyAnimator:
         self.ax = None
         self.cont = None
 
-    def set_densities(self, nlevels_min=100, quantile=0.95):
+    def set_densities(self):
         densities = []
         if self.frame_data is None:
             raise Exception("Frame data not set")
@@ -67,11 +198,14 @@ class MyAnimator:
                 trajectory_length=traj_len0,
             )
             densities.append(od)
-        self.densities = np.stack(densities)
-        self.vmax = np.nanquantile(self.densities, quantile)
-        self.nlevels = min(nlevels_min, int(density_eval_locations.shape[0] / 2))
-        self.targets = targets  # these should be the same each time
         self.density_eval_locations = density_eval_locations
+        self.targets = targets # should be the same each time
+        self.densities = np.stack(densities)
+
+    def set_contour_prefs(self, nlevels_min=100, max_quantile=0.95, min_quantile=0.05):
+        self.vmax = np.nanquantile(self.densities, max_quantile)
+        self.vmin = np.nanquantile(self.densities, min_quantile)
+        self.nlevels = min(nlevels_min, int(self.density_eval_locations.shape[0] / 2))
         LOG.info(f"Using {self.nlevels} levels with vmax={self.vmax:.2f}")
         LOG.info(f"Density shape: {self.densities.shape}")
 
@@ -99,12 +233,17 @@ class MyAnimator:
 
     def set_likelihoods(self):
         with h5py.File(self.density_loc, "a") as f:
+            print(self.num_contexts)
+            print(f[Groups.MARGINAL_DENSITIES.value].keys())
             gname = f"{self.num_contexts}|{self.func_ind}"
             grp = f[Groups.MARGINAL_DENSITIES.value][gname]
             tgrp = f[Groups.TRAJECTORIES.value]
+            print(tgrp.keys())
+            # tmp_t = 0 # <- TODO: this has to be fixed, don't save this!
+            # t0 = tgrp[f"{tmp_t}"]
             t0 = tgrp[f"{self.num_contexts}"]
 
-            lh = grp[Datasets.LIKELIHOODS.value][:]
+            lh = grp[Datasets.LOG_LIKELIHOODS.value][:]
             xt = grp["x_targets"][:]
             eval_points = grp["y_density_evaluation_points"][:]
             cx = t0["cx"][self.func_ind, :]
@@ -112,22 +251,36 @@ class MyAnimator:
 
         self.lh = lh
         self.xt = xt
-        self.eval_points = eval_points
+        # self.eval_points = eval_points
         self.true_y_targets = eval_points[:, 0]
         self.cx = cx
         self.cy = cy
         self.total_num_trajectories = lh.shape[1]
         self.total_trajectory_len = lh.shape[-1]
 
+        self.means, self.variances, _, _ = get_function_predictive_params(
+            self.density_loc,
+            self.num_contexts,
+            self.func_ind,
+        )
+
     def get_density_grid(self, num_trajectories, trajectory_length):
-        density = self.lh[:, :num_trajectories, :, trajectory_length]
-        gmm = density.mean(axis=1).T
+        # density = self.lh[:, :num_trajectories, :, trajectory_length]
+        gmm = get_function_density(
+            self.means,
+            self.variances,
+            self.xt,
+            num_trajectories,
+            trajectory_length,
+            self.eval_points,
+        )
+        # gmm = density.mean(axis=1).T
         x_order = np.argsort(self.xt, axis=0).reshape(-1)
-        y_order = np.argsort(-self.eval_points[0], axis=0).reshape(-1)
+        y_order = np.argsort(-self.eval_points, axis=0).reshape(-1)
         od = gmm[:, x_order][y_order, :]
 
         targets = self.xt[x_order].reshape(-1)
-        density_eval_locations = self.eval_points[0][y_order].reshape(-1)
+        density_eval_locations = self.eval_points[y_order]
         return od, targets, density_eval_locations
 
     def set_first_frame(self):
@@ -146,7 +299,7 @@ class MyAnimator:
         x = targets
         y = density_eval_locations
         self.cont = plt.contourf(
-            x, y, od, self.nlevels, vmax=self.vmax, zorder=1
+            x, y, od, self.nlevels, vmax=self.vmax, vmin=self.vmin, zorder=1
         )  # first image on screen
         plt.scatter(
             self.cx, self.cy, s=150, marker="+", color="red", label="contexts", zorder=3
@@ -174,12 +327,14 @@ class MyAnimator:
             raise Exception("First frame not set")
         for c in self.cont.collections:
             c.remove()  # removes only the contours, leaves the rest intact
+        # There is a lot duplicate code in here
         cont = plt.contourf(
             targets,
             density_eval_locations,
             od,
             self.nlevels,
             cmap="viridis",
+            vmin=self.vmin,
             vmax=self.vmax,
             zorder=1,
         )
