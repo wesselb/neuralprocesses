5b9ea42d5d73a18dbcf9d5f049731599faf9cb51
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-09-12 16:17:05 +0100

    Support broadcasting for density channels

--- neuralprocesses/coders/setconv/density.py
+++ neuralprocesses/coders/setconv/density.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 
 import lab as B
 
@@ -10,20 +10,46 @@ from ...util import register_module, batch
 
 __all__ = [
     "PrependDensityChannel",
+    "PrependMultiDensityChannel",
     "DivideByFirstChannel",
+    "DivideByFirstHalf",
 ]
 
 
 @register_module
 class PrependDensityChannel:
-    """Prepend a density channel to the current encoding."""
+    """Prepend a density channel to the current encoding.
+
+    Args:
+        multi (bool, optional): Produce a separate density channel for every channel.
+            If `False`, produce just one density channel for all channels. Defaults
+            to `False`.
+    """
+
+    def __init__(self, multi=False):
+        self.multi = multi
+
+
+@register_module
+class PrependMultiDensityChannel(PrependDensityChannel):
+    """Prepend a separate density channel for every channel to the current encoding."""
+
+    def __init__(self):
+        PrependDensityChannel.__init__(self, multi=True)
 
 
 @_dispatch
 def code(coder: PrependDensityChannel, xz, z: B.Numeric, x, **kw_args):
     d = data_dims(xz)
     with B.on_device(z):
-        density_channel = B.ones(B.dtype(z), *batch(z, d + 1), 1, *B.shape(z)[-d:])
+        if coder.multi:
+            # Produce separate density channels.
+            c = B.shape(z, -d - 1)
+        else:
+            # Produce just one density channel.
+            c = 1
+        print(c, coder.multi, d, B.shape(z, -d - 1), B.shape(z))
+        density_channel = B.ones(B.dtype(z), *batch(z, d + 1), c, *B.shape(z)[-d:])
     return xz, B.concat(density_channel, z, axis=-d - 1)
 
 
@@ -37,11 +63,30 @@ def code(coder: PrependDensityChannel, xz, z: Masked, x, **kw_args):
     # do the rest for us.
     mask = z.mask
     xz, z = code(coder, xz, z.y, x, **kw_args)
-    return xz, (mask * z)
+    return xz, mask * z
 
 
 @register_module
-class DivideByFirstChannel:
+class DivideByChannels:
+    """Divide by the first `n` channels.
+
+    Args:
+        spec (int or str): Channels to divide by.
+        epsilon (float): Value to add to the channel before dividing.
+
+    Attributes:
+        spec (int or str): Channels to divide by.
+        epsilon (float): Value to add to the channel before dividing.
+    """
+
+    @_dispatch
+    def __init__(self, spec: Union[int, str], epsilon: float):
+        self.spec = spec
+        self.epsilon = epsilon
+
+
+@register_module
+class DivideByFirstChannel(DivideByChannels):
     """Divide by the first channel.
 
     Args:
@@ -53,12 +98,28 @@ class DivideByFirstChannel:
 
     @_dispatch
     def __init__(self, epsilon: float = 1e-8):
-        self.epsilon = epsilon
+        DivideByChannels.__init__(self, 1, epsilon)
+
+
+@register_module
+class DivideByFirstHalf(DivideByChannels):
+    """Divide by the first half of channels.
+
+    Args:
+        epsilon (float): Value to add to the channels before dividing.
+
+    Attributes:
+        epsilon (float): Value to add to the channels before dividing.
+    """
+
+    @_dispatch
+    def __init__(self, epsilon: float = 1e-8):
+        DivideByChannels.__init__(self, "half", epsilon)
 
 
 @_dispatch
 def code(
-    coder: DivideByFirstChannel,
+    coder: DivideByChannels,
     xz,
     z: B.Numeric,
     x,
@@ -67,16 +128,22 @@ def code(
 ):
     epsilon = epsilon or coder.epsilon
     d = data_dims(xz)
-    slice_to_one = (Ellipsis, slice(None, 1, None)) + (slice(None, None, None),) * d
-    slice_from_one = (Ellipsis, slice(1, None, None)) + (slice(None, None, None),) * d
+    if isinstance(coder.spec, B.Int):
+        num_divide = coder.spec
+    elif coder.spec == "half":
+        num_divide = B.shape(z, -d - 1) // 2
+    else:
+        raise ValueError(f"Unknown specification `{coder.spec}`.")
+    slice1 = (Ellipsis, slice(None, num_divide, None)) + (slice(None, None, None),) * d
+    slice2 = (Ellipsis, slice(num_divide, None, None)) + (slice(None, None, None),) * d
     return (
         xz,
         B.concat(
-            z[slice_to_one],
-            z[slice_from_one] / (z[slice_to_one] + epsilon),
+            z[slice1],
+            z[slice2] / (z[slice1] + epsilon),
             axis=-d - 1,
         ),
     )
 
 
-broadcast_coder_over_parallel(DivideByFirstChannel)
+broadcast_coder_over_parallel(DivideByChannels)
