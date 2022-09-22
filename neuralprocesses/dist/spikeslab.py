f344599210b48d31d20151a1438c98c8712223bf
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-09-22 08:42:57 +0200

    Add prototype `logpdf` method

--- neuralprocesses/dist/spikeslab.py
+++ neuralprocesses/dist/spikeslab.py
@@ -15,21 +15,34 @@ class SpikesAndSlab(AbstractDistribution):
         slab (:class:`neuralprocesses.dist.dist.AbstractDistribution`): Slab.
         logprobs (tensor): Log-probabilities for the spikes and the slab with the
             log-probability for the slab last.
+        epsilon (float, optional): Error to check equality with. Defaults to `1e-6`.
+        safe_slab_value (float, optional): Safe value to to evaluate the slab at.
+            Defaults to `0.5`.
 
     Attributes:
         spikes (vector): Spikes.
         slab (:class:`neuralprocesses.dist.dist.AbstractDistribution`): Slab.
         logprobs (tensor): Log-probabilities for the spikes and the slab with the
             log-probability for the slab last.
+        epsilon (float): Error to check equality with.
+        safe_slab_value (float): Safe value to to evaluate the slab at.
     """
 
     @_dispatch
     def __init__(
-        self, spikes: B.Numeric, slab: AbstractDistribution, logprobs: B.Numeric
+        self,
+        spikes: B.Numeric,
+        slab: AbstractDistribution,
+        logprobs: B.Numeric,
+        *,
+        epsilon: float = 1e-6,
+        safe_slab_value: float = 0.5,
     ):
         self.spikes = spikes
         self.slab = slab
         self.logprobs = logprobs
+        self.epsilon = epsilon
+        self.safe_slab_value = safe_slab_value
 
     def __repr__(self):
         return (  # Comment to preserve formatting.
@@ -44,7 +57,30 @@ class SpikesAndSlab(AbstractDistribution):
         )
 
     def logpdf(self, x):
-        pass
+        # Construct indicators which filter out the spikes.
+        spikes_indicator = B.cast(
+            B.dtype(x),
+            B.abs(
+                B.expand_dims(self.spikes, axis=0, times=B.rank(x))
+                - B.expand_dims(x, axis=-1, times=1)
+            )
+            < self.epsilon,
+        )
+        # Construct an indicator for the slab.
+        with B.on_device(x):
+            any_spike = B.minimum(B.sum(spikes_indicator, axis=-1), B.one(x))
+            slab_indicator = B.one(x) - any_spike
+        # Compute the log-probability of the categorical random variable.
+        full_indicator = B.concat(spikes_indicator, slab_indicator[..., None], axis=-1)
+        cat_logprob = B.sum(full_indicator * self.logprobs, axis=-1)
+        # Compute the log-probability of the slab.
+        with B.on_device(x):
+            safe = B.to_active_device(B.cast(B.dtype(x), self.safe_slab_value))
+            slab_logprob = slab_indicator * self.slab.logpdf(
+                slab_indicator * x + safe * (B.one(x) - slab_indicator)
+            )
+        # Assemble the full log-probability and return.
+        return cat_logprob + slab_logprob
 
     @_dispatch
     def sample(self, state: B.RandomState, dtype: B.DType, *shape):
